a88bbcb797756ffbd6935d3e490a685e754de1a3
Marmik <marmikpatel262@gmail.com>
2026-02-23 12:01:47 -0600

    Fix duplicate import of Script in layout.tsx
    Removed duplicate import of Script from 'next/script'.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import '@/styles/globals.css'
 import { Header } from '@/app/components/Header'
 import { Footer } from '@/app/components/Footer'
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'Marmik Patel | Senior Solutions Architect / Senior Data Engineer',
@@ -13,10 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <!-- Cloudflare Web Analytics -->
           <script defer
             strategy="afterInteractive"  src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "8e05ac02362d4dc9ae87ba596b06f288"}'></script>
-        <!-- End Cloudflare Web Analytics -->
       </head>
       <body>
         <div className="min-h-screen">
